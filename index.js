a339d1bf0d58a58e17be462a3ce36c2e64dcdd43
ghwdev <ghwdev8@gmail.com>
2024-11-06 17:30:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+$(".botao").click(function(){
+let personagem =$("#personagem").val()
+$(".pn").css("display","block")
+
+
+function alerta(personagem) {
+var imagem = document.createElement("img")
+var srcc = "https://img.freepik.com/fotos-premium/retrato-antropomorfico-de-um-empresario-lobo-lobo-chefe-em-terno-de-estilo-oficial_370610-392.jpg?w=826"
+imagem.src = srcc;
+alert("nao")
+document.body.appendChild(imagem)
+var textos = document.querySelectorAll(".tudo");
+textos.forEach(function(elemento) {
+    elemento.innerHTML = "O seu personagem é :" + personagem;
+});
+
+
+}
+
+setTimeout(function(){ alerta(personagem)}, 5000);
+})
